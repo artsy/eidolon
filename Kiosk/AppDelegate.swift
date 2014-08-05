c14202fb4a5806c89c7e752881dfcb7cd8b8f75c
Ash Furrow <ash@ashfurrow.com>
2014-08-05 19:15:11 +0200

    Removed storyboard.

--- Kiosk/AppDelegate.swift
+++ Kiosk/AppDelegate.swift
@@ -11,11 +11,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
                             
-    var window: UIWindow?
+    var window: UIWindow!
 
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window.rootViewController = ViewController(nibName: nil, bundle: nil)
+        window.makeKeyAndVisible()
+        
         return true
     }
 
