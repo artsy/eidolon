28625ad626784546479d2ecf2595c4031c55ecac
Ash Furrow <ash@ashfurrow.com>
2014-08-07 16:09:55 +0200

    Updated KioskTests for use with Quick/Nimble.

--- KioskTests/KioskTests.swift
+++ KioskTests/KioskTests.swift
@@ -6,34 +6,15 @@
 //  Copyright (c) 2014 Artsy. All rights reserved.
 //
 
-//import Quick
-//import Nimble
+import Quick
+import Nimble
 
-import UIKit
-import XCTest
-
-class KioskTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class KioskSpec: QuickSpec {
+    override func spec() {
+        describe("in some context", { () -> () in
+            it("passes a basic assertion") {
+                expect(1).to(equal(1))
+            }
+        });
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
