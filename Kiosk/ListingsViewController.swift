e0b6a3b054b46d7227d358dab61e4283b6d2908a
Orta Therox <orta.therox@gmail.com>
2014-09-05 11:19:28 -0400

    remove header comments from files

--- Kiosk/ListingsViewController.swift
+++ Kiosk/ListingsViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ListingsViewController.swift
-//  Kiosk
-//
-//  Created by Ash Furrow on 2014-08-05.
-//  Copyright (c) 2014 Artsy. All rights reserved.
-//
-
 import UIKit
 
 public class ListingsViewController: UIViewController {
--- Kiosk/PlaceBidViewController.swift
+++ Kiosk/PlaceBidViewController.swift
@@ -1,11 +1,3 @@
-//
-//  PlaceBidViewController.swift
-//  Kiosk
-//
-//  Created by Orta on 9/5/14.
-//  Copyright (c) 2014 Artsy. All rights reserved.
-//
-
 import UIKit
 
 public class PlaceBidViewController: UIViewController {
