ca098147e09657b43e834548116c2f982749a700
Orta Therox <orta.therox@gmail.com>
2014-09-24 17:20:19 +0100

    add some simple models around auctions

---
+++ Kiosk/App/Models/SaleArtwork.swift
@@ -0,0 +1,13 @@
+//
+//  SaleArtwork.swift
+//  Kiosk
+//
+//  Created by Orta on 24/09/2014.
+//  Copyright (c) 2014 Artsy. All rights reserved.
+//
+
+import UIKit
+
+class SaleArtwork: NSObject {
+   
+}
