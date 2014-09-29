f5e6003caac751f8ed4589b366fbd08abbe1cb9e
Ash Furrow <ash@ashfurrow.com>
2014-09-29 17:55:11 +0200

    Initial work.

--- Kiosk/ListingsViewController.swift
+++ Kiosk/ListingsViewController.swift
@@ -1,36 +1,105 @@
 import UIKit
 
+let CellIdentifier = "Cell"
+
 class ListingsViewController: UIViewController {
-    var allowAnimations:Bool = true;
+    var allowAnimations = true
+    var salesArtworks = [SaleArtwork]()
+    
+    lazy var collectionView: UICollectionView = {
+        var collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.backgroundColor = UIColor.clearColor()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: CellIdentifier)
+        return collectionView
+    }()
+    lazy var switchView: SwitchView = {
+        return SwitchView(buttonTitles: SwitchValues.allSwitchValues().map{$0.name.uppercaseString})
+    }()
 
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        self.showModal(self)
-    }
-
-    @IBAction func showModal(sender: AnyObject) {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add subviews
+        view.addSubview(switchView)
+        view.addSubview(collectionView)
+        
+        // Set up reactive bindings
         let endpoint: ArtsyAPI = ArtsyAPI.AuctionListings(id: "ici-live-auction")
 
-        XAppRequest(endpoint, parameters: endpoint.defaultParameters).filterSuccessfulStatusCodes().mapJSON().mapToObjectArray(SaleArtwork.self).subscribeNext({ [weak self] (objects) -> Void in
-
-            let saleArtworks = objects as [SaleArtwork]
-            let storyboard = UIStoryboard.fulfillment()
-            let containerController = storyboard.instantiateInitialViewController() as FulfillmentContainerViewController
-
-            if let placeBidVC = containerController.placeBidViewController() {
-                let randomIndex = Int(arc4random_uniform(UInt32(saleArtworks.count)))
-                placeBidVC.saleArtwork = saleArtworks[randomIndex]
-            }
-
-            self?.presentViewController(containerController, animated: self!.allowAnimations, completion: nil)
-
-        }, error: { (error) -> Void in
-            println("Error: \(error.localizedDescription)")
-        })
-        
-
+        RAC(self, "salesArtworks") <~ XAppRequest(endpoint, parameters: endpoint.defaultParameters).filterSuccessfulStatusCodes().mapJSON().mapToObjectArray(SaleArtwork.self).doNext { [unowned self] (objects) -> Void in
+            self.collectionView.reloadData()
+        }
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        let switchHeightPredicate = "\(switchView.intrinsicContentSize().height)"
+        
+        switchView.constrainHeight(switchHeightPredicate)
+        switchView.alignTop("64", leading: "0", bottom: nil, trailing: "0", toView: view)
+        collectionView.constrainTopSpaceToView(switchView, predicate: "0")
+        collectionView.alignTop(nil, leading: "0", bottom: "0", trailing: "0", toView: view)
+    }
 }
 
+// MARK: - Collection View
+
+extension ListingsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return countElements(salesArtworks)
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(CellIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+        
+        cell.backgroundColor = UIColor.blackColor()
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        collectionView.deselectItemAtIndexPath(indexPath, animated: true)
+        
+        let storyboard = UIStoryboard.fulfillment()
+        let containerController = storyboard.instantiateInitialViewController() as FulfillmentContainerViewController
+        
+        if let placeBidViewController = containerController.placeBidViewController() {
+            placeBidViewController.saleArtwork = salesArtworks[indexPath.row]
+        }
+        
+        self.presentViewController(containerController, animated: self.allowAnimations, completion: nil)
+    }
+}
+
+// MARK: - Switch Values
+
+enum SwitchValues {
+    case Grid
+    case LeastBids
+    case MostBids
+    case HighestCurrentBid
+    case LowestCurrentBid
+    case Alphabetical
+    
+    var name: String {
+        switch self {
+        case .Grid:
+            return "Grid"
+        case .LeastBids:
+            return "Least Bids"
+        case .MostBids:
+            return "MostBids"
+        case .HighestCurrentBid:
+            return "Highest Current Bid"
+        case .LowestCurrentBid:
+            return "Lowest Current Bid"
+        case .Alphabetical:
+            return "A—Z"
+            }
+    }
+    
+    static func allSwitchValues() -> [SwitchValues] {
+        return [Grid, LeastBids, MostBids, HighestCurrentBid, LowestCurrentBid, Alphabetical]
+    }
+}
