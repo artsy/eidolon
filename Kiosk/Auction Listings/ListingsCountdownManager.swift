7c9813c0db0f4ffe15a9f8a800fc945a1ba5a99a
Laura Brown <laurabrown1113@gmail.com>
2014-10-15 13:33:06 -0400

    counter numbers are always >=2 digits

--- Kiosk/Auction Listings/ListingsCountdownManager.swift
+++ Kiosk/Auction Listings/ListingsCountdownManager.swift
@@ -4,13 +4,15 @@ class ListingsCountdownManager: NSObject {
    
     @IBOutlet weak var countdownLabel: UILabel!
     @IBOutlet var countdownContainerView: UIView!
+    let formatter = NSNumberFormatter()
 
     dynamic var sale: Sale?
 
     let time = SystemTime()
     
     override func awakeFromNib() {
-        
+        formatter.minimumIntegerDigits = 2
+
         time.syncSignal().subscribeNext { [weak self] (_) in
             self?.startTimer()
             return
@@ -47,7 +49,7 @@ class ListingsCountdownManager: NSObject {
                 let flags: NSCalendarUnit = .CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond
                 let components = NSCalendar.currentCalendar().components(flags, fromDate: now, toDate: sale.endDate, options: nil)
                 
-                self.countdownLabel.text = "\(components.hour) : \(components.minute) : \(components.second)"
+                self.countdownLabel.text = "\(formatter.stringFromNumber(components.hour)!) : \(formatter.stringFromNumber(components.minute)!) : \(formatter.stringFromNumber(components.second)!)"
 
             } else {
                 self.countdownLabel.text = "CLOSED"
