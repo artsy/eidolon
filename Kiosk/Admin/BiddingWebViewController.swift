fac39cb37d6020508a439a4e26da4e8bb0f9fc8d
Orta Therox <orta.therox@gmail.com>
2014-10-15 23:15:41 -0400

    web bidders?

---
+++ Kiosk/Admin/BiddingWebViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BiddingWebViewController.swift
+//  Kiosk
+//
+//  Created by Orta on 10/15/14.
+//  Copyright (c) 2014 Artsy. All rights reserved.
+//
+
+import UIKit
+
+class BiddingWebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
