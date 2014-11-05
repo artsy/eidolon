3a3c99ee56d65a87e0987f404e19f9e67b605854
Ash Furrow <ash@ashfurrow.com>
2014-11-05 20:46:07 +0100

    Fixed another crasher.

--- Kiosk/Bid Fulfillment/BidderNetworkModel.swift
+++ Kiosk/Bid Fulfillment/BidderNetworkModel.swift
@@ -50,7 +50,7 @@ class BidderNetworkModel: NSObject {
 
     private func createNewUser() -> RACSignal {
         let newUser = details().newUser
-        let endpoint: ArtsyAPI = ArtsyAPI.CreateUser(email: newUser.email!, password: newUser.password!, phone: newUser.phoneNumber!, postCode: newUser.zipCode!, name: newUser.name!)
+        let endpoint: ArtsyAPI = ArtsyAPI.CreateUser(email: newUser.email!, password: newUser.password!, phone: newUser.phoneNumber!, postCode: newUser.zipCode!, name: newUser.name ?? "")
 
         return Provider.sharedProvider.request(endpoint, method: .POST, parameters: endpoint.defaultParameters).filterSuccessfulStatusCodes().doError { (error) in
             logger.error("Creating user failed.")
