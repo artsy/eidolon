91a2b3adab91988d4a6a08877f0bcee7059fe217
Ash Furrow <ash@ashfurrow.com>
2015-09-22 15:06:36 -0400

    [CC Processing] Always use the card token, even on staging.

--- Kiosk/Bid Fulfillment/SwipeCreditCardViewController.swift
+++ Kiosk/Bid Fulfillment/SwipeCreditCardViewController.swift
@@ -62,11 +62,7 @@ class SwipeCreditCardViewController: UIViewController, RegistrationSubController
                 self.cardName = card.name
                 self.cardLastDigits = card.encryptedSwipedCardNumber
 
-                if AppSetup.sharedState.useStaging {
-                    self.cardToken = "/v1/marketplaces/TEST-MP7Fs9XluC54HnVAvBKSI3jQ/cards/CC1AF3Ood4u5GdLz4krD8upG"
-                } else {
-                    self.cardToken = card.cardToken
-                }
+                self.cardToken = card.cardToken
 
                 // TODO: RACify this
                 if let newUser = self.navigationController?.fulfillmentNav().bidDetails.newUser {
