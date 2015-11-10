daf5035e40707eb95a76885be14c7f555132eb79
Ash Furrow <ash@ashfurrow.com>
2015-11-10 16:57:26 -0500

    [FRP] Removed old files.

--- Kiosk/UITextField+RAC.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-import RxSwift
-
-extension UITextField {
-    func returnKeySignal () -> RACSignal {
-        return rac_signalForControlEvents(.EditingDidEndOnExit).takeUntil(rac_willDeallocSignal())
-    }
-}
