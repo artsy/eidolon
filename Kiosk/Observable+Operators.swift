9a7f353390af6290d22851cf7aa4b12a66c10e75
Ash Furrow <ash@ashfurrow.com>
2015-11-10 22:11:22 -0500

    [FRP] Removes dead code.

--- Kiosk/Observable+Operators.swift
+++ Kiosk/Observable+Operators.swift
@@ -1,19 +1,5 @@
 import RxSwift
 
-//extension Observable {
-//    class func ifTrue<E>(condition: Observable<Bool>, then: Observable<E>, otherwise: Observable<E>) -> Observable<E> {
-//        return deferred { () -> Observable<E> in
-//            condition.take(1).flatMap({ (c) -> Observable<E> in
-//                if c {
-//                    return then
-//                } else {
-//                    return otherwise
-//                }
-//            })
-//        }
-//    }
-//}
-
 extension Observable where Element: Equatable {
     func ignore(value: Element) -> Observable<Element> {
         return filter({ (e) -> Bool in
