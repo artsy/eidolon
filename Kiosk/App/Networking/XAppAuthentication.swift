22211783375c9f6871896b461a3dc19e876441cc
Ash Furrow <ash@ashfurrow.com>
2015-11-10 22:11:34 -0500

    [FRP] Shores up authentication.

--- Kiosk/App/Networking/XAppAuthentication.swift
+++ Kiosk/App/Networking/XAppAuthentication.swift
@@ -5,34 +5,46 @@ import RxSwift
 
 /// Request to fetch and store new XApp token if the current token is missing or expired.
 
-private func XAppTokenRequest(defaults: NSUserDefaults) -> Observable<AnyObject> {
+private func XAppTokenRequest(defaults: NSUserDefaults) -> Observable<String?> {
 
-    // I don't like an extension of a class referencing what is essentially a singleton of that class.
     var appToken = XAppToken(defaults: defaults)
 
-    let newTokenRequest = Provider.sharedProvider.request(ArtsyAPI.XApp).filterSuccessfulStatusCodes().mapJSON().doOn { event in
-        if let dictionary = event.element as? NSDictionary {
-            let formatter = ISO8601DateFormatter()
-            appToken.token = dictionary["xapp_token"] as? String
-            appToken.expiry = formatter.dateFromString(dictionary["expires_in"] as? String)
-        }
-
-    }.logError().ignoreElements()
-
+    // If we have a valid token, return it and forgo a request for a fresh one.
     if appToken.isValid {
-        return just(appToken.token!)
-    } else {
-        return newTokenRequest
+        return just(appToken.token)
     }
+
+    let newTokenRequest = Provider.sharedProvider.request(ArtsyAPI.XApp)
+        .filterSuccessfulStatusCodes()
+        .mapJSON()
+        .map { element -> (token: String?, expiry: String?) in
+            guard let dictionary = element as? NSDictionary else { return (token: nil, expiry: nil) }
+
+            return (token: dictionary["xapp_token"] as? String, expiry: dictionary["expires_in"] as? String)
+        }
+        .doOn { event in
+            guard case Event.Next(let element) = event else { return }
+
+            let formatter = ISO8601DateFormatter()
+            appToken.token = element.0
+            appToken.expiry = formatter.dateFromString(element.1)
+        }
+        .map { (token, expiry) -> String? in
+            return token
+        }
+        .logError()
+
+    return newTokenRequest
 }
 
 /// Request to fetch a given target. Ensures that valid XApp tokens exist before making request
 func XAppRequest(token: ArtsyAPI, provider: ArtsyProvider<ArtsyAPI> = Provider.sharedProvider, defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()) -> Observable<MoyaResponse> {
 
-    // TODO: Incorrect, shouldn't use flatMap since XAppTokenRequest sends no values.
-    return provider.online.ignore(false).take(1).flatMap({ (_) -> Observable<MoyaResponse> in
-        return XAppTokenRequest(defaults).flatMap({ (_) -> Observable<MoyaResponse> in
-            return provider.request(token)
-        })
-    })
+    return provider.online
+        .ignore(false)  // Wait unti we're online
+        .take(1)        // Take 1 to make sure we only invoke the API once.
+        .flatMap { _ in // Turn the online state into a network request
+            return XAppTokenRequest(defaults).map { _ in provider.request(token) }
+        .switchLatest() // Subscribe to the network request
+    }
 }
