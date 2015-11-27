f99c549dec754191822e00c86c0905317a8f0668
Ash Furrow <ash@ashfurrow.com>
2015-11-27 15:42:08 -0500

    [FRP] Remaining subjects.

--- Kiosk/Bid Fulfillment/RegistrationMobileViewController.swift
+++ Kiosk/Bid Fulfillment/RegistrationMobileViewController.swift
@@ -5,23 +5,41 @@ class RegistrationMobileViewController: UIViewController, RegistrationSubControl
     
     @IBOutlet var numberTextField: TextField!
     @IBOutlet var confirmButton: ActionButton!
-    let finishedSignal = RACSubject()
+    let finished = PublishSubject<Void>()
 
     lazy var viewModel: GenericFormValidationViewModel = {
-        let numberIsValidSignal = self.numberTextField.rac_textSignal().map(isZeroLengthString).not()
-        return GenericFormValidationViewModel(isValidSignal: numberIsValidSignal, manualInvocationSignal: self.numberTextField.returnKeySignal(), finishedSubject: self.finishedSignal)
+        let numberIsValidSignal = self.numberTextField.rx_text.map(isZeroLengthString).not()
+        return GenericFormValidationViewModel(isValidSignal: numberIsValidSignal, manualInvocationSignal: self.numberTextField.returnKeySignal(), finishedSubject: self.finished)
     }()
+
+    private let _viewWillDisappear = PublishSubject<Void>()
+    var viewWillDisappear: Observable<Void> {
+        return self._viewWillDisappear.asObserver()
+    }
 
     lazy var bidDetails: BidDetails! = { self.navigationController!.fulfillmentNav().bidDetails }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        numberTextField.text = bidDetails.newUser.phoneNumber
-        RAC(bidDetails, "newUser.phoneNumber") <~ numberTextField.rac_textSignal().takeUntil(viewWillDisappearSignal())
-        confirmButton.rac_command = viewModel.command
+        numberTextField.text = bidDetails.newUser.phoneNumber.value
+        numberTextField
+            .rx_text
+            .asObservable()
+            .mapToOptional()
+            .takeUntil(viewWillDisappear)
+            .bindTo(bidDetails.newUser.phoneNumber)
+            .addDisposableTo(rx_disposeBag)
+
+        confirmButton.rx_action = viewModel.command
 
         numberTextField.becomeFirstResponder()
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        _viewWillDisappear.onNext()
     }
 
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
